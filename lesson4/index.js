aeef2316042bc6b45cfdcc98492b6285f2b31fbe
Adilet Sataev <adilet.sataev.kg@gmail.com>
2021-11-14 16:02:24 +0600

    clean lesson4

--- lesson4/index.js
+++ lesson4/index.js
@@ -1,29 +1,31 @@
 // return
 function square(a) {
+    // Выводим результат
+    console.log(a * a)
+    // возвращаем результат для дальшейших работы
     return a * a
 }
-
+// Если return отсутствует, в переменную запишется undefined
 var mySquare = square(5)
 
 console.log(mySquare)
 
 
+// Дз 2 урока 2 задание
 function countChar(str, subStr, str1) {
-    console.log(str)
-    console.log(subStr)
+    // мы ничего не передавали в str1, поэтому в нём undefined
     console.log(str1)
-    // var counter = 0
-    // for (var i = 0; i < str.length; i++) {
-    //     if (str[i] === subStr) {
-    //         counter++
-    //     }
-    // }
-    // // console.log(counter)
-    // return counter
+    var counter = 0
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === subStr) {
+            counter++
+        }
+    }
+    return counter
 }
 
 
-countChar('dadw', 'edaed', 'dadead')
+console.log(countChar('d', 'edaedda'))
 
 function code () {
     console.log('Кодить')
@@ -31,11 +33,26 @@ function code () {
 
 var programmer = {
     level: 'junion',
+    // метод
     code: function () {
         console.log('Кодить')
     }
 }
 
+// вызов метода
+programmer.code()
+
+
+var array = [1, 2, 3, 54, 546, 2389, 23, 54, 1]
+
+// map, filter, reduce
+// Функция, которая передаётся как аргумент или возвращается другой функцией
+// назыввается callback-ом
+
+
+// Filter
+
+// функция callback для filter
 function isEven(num) {
     var even = num % 2 === 0
     if (even) {
@@ -45,22 +62,10 @@ function isEven(num) {
     }
 }
 
-function multiply(num) {
-    return num * 2
-}
-
-function add(sum, num) {
-    return sum + num
-}
-
-programmer.code()
-
-var array = [1, 2, 3, 54, 546, 2389, 23, 54, 1]
-console.log(array)
-// map, filter, reduce
-// Функция, которая передаётся как аргумент или возвращается другой функцией
-// назыввается callback-ом
 var массивСЧётными = array.filter(isEven)
+console.log(массивСЧётными)
+
+// самодельный фильт
 function filter(arr, callback) {
     var newArr = []
     for (var i = 0; i < arr.length; i++) {
@@ -74,9 +79,29 @@ function filter(arr, callback) {
 console.log(filter(array, isEven))
 
 
+
+
+
+// Map
+
+// функция callback для map
+function multiply(num) {
+    return num * 2
+}
+
 var multiplied = array.map(multiply)
-// console.log(массивСЧётными)
-var reduced = array.reduce(add)
+console.log(array, multiplied)
+
+
+
+// Reduce
+
+// функция callback для reduce
+function add(sum, num) {
+    return sum + num
+}
+
+// самодельная функция reduce
 function reduce(arr, callback) {
     var sum = 0
     for (var i = 0; i < arr.length; i++) {
@@ -84,10 +109,15 @@ function reduce(arr, callback) {
     }
     return sum
 }
+
 console.log(reduce(array, add))
 
 
-var blueSquare = document.getElementById('square')
-blueSquare.style.background = 'red'
+// метод reduce
+var reduced = array.reduce(add)
+console.log(reduced)
 
-console.log('test')
+// Получение div-a с id "square"
+var blueSquare = document.getElementById('square')
+// Меняем ему цвет фона на красный
+blueSquare.style.background = 'red'
